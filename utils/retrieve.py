faaf1f5c43f9a414d1d7236ac89793132f3356d6
IsaiasZc <coroxerox2@gmail.com>
2024-09-06 14:39:41 -0500

    [UPDATE] optimizatrion first try

--- utils/retrieve.py
+++ utils/retrieve.py
@@ -1,5 +1,4 @@
 import re
-import os
 
 import chromadb
 from llama_index.core import VectorStoreIndex
@@ -11,12 +10,8 @@ from llama_index.core.node_parser import (
 )
 from llama_index.core.vector_stores.types import VectorStoreQueryMode
 from llama_index.vector_stores.chroma import ChromaVectorStore
-from llama_index.vector_stores.milvus import MilvusVectorStore
-# from llama_index.vector_stores.qdrant import QdrantVectorStore
-from llama_index.embeddings.huggingface import HuggingFaceEmbedding
+# from llama_index.embeddings.huggingface import HuggingFaceEmbedding
 from llama_index.embeddings.openai import OpenAIEmbedding
-from llama_index.embeddings.voyageai import VoyageEmbedding
-from pymilvus import MilvusClient
 
 
 def _generate_ngrams_from_text(text, ngram_size=3):
@@ -116,14 +111,14 @@ def extract_question_ngrams(qa_df, ngram_size):
     """
     question_ngrams = {}
     for _, row in qa_df.iterrows():
-        question = row["Questions"]
+        question = row["Question"]
         all_ngrams = []
         for column in qa_df.columns:
             if column in [
                 "Initials",
-                "Questions",
+                "Question",
                 "Ideal Answer",
-                "Link to Ideal Answer",
+                "Link",
             ]:
                 continue
             text = row[column]
@@ -151,27 +146,19 @@ def objective(trial, documents, ngram_size, question_ngrams, f_beta=1.0):
     embed_model_name = trial.suggest_categorical(
         "embed_model",
         [
-            "text-embedding-3-small",
-            "voyage-large-2-instruct",
+            "text-embedding-3-large",
+            # "voyage-large-2-instruct",
             # GPU runs out of memory with gte model
             # 'Alibaba-NLP/gte-large-en-v1.5',  # WARNING: this downloads 1.74G of data
         ],
     )
-    if embed_model_name == "text-embedding-3-small":
+    if embed_model_name == "text-embedding-3-large":
         embed_model = OpenAIEmbedding(
             model=embed_model_name,
             embed_batch_size=10,
             max_retries=25,
             timeout=180,
             reuse_client=False,
-        )
-    elif embed_model_name == "voyage-large-2-instruct":
-        embed_model = VoyageEmbedding(
-            model_name=embed_model_name,
-        )
-    elif embed_model_name == "Alibaba-NLP/gte-large-en-v1.5":
-        embed_model = HuggingFaceEmbedding(
-            model_name=embed_model_name, trust_remote_code=True
         )
 
     # define splitter
@@ -183,6 +170,11 @@ def objective(trial, documents, ngram_size, question_ngrams, f_beta=1.0):
             "markdown",
         ],
     )
+
+    include_prev_next_rel = trial.suggest_categorical(
+            "include_prev_next_rel", [True, False]
+        )
+
     if splitter_name == "sentence":
         chunk_size = trial.suggest_int("chunk_size", 256, 1024)
         chunk_overlap = trial.suggest_int("chunk_overlap", 0, 200)
@@ -192,9 +184,6 @@ def objective(trial, documents, ngram_size, question_ngrams, f_beta=1.0):
         breakpoint_percentile_threshold = trial.suggest_int(
             "breakpoint_percentile_threshold", 60, 95
         )
-        include_prev_next_rel = trial.suggest_categorical(
-            "include_prev_next_rel", [True, False]
-        )
         splitter = SemanticSplitterNodeParser(
             buffer_size=buffer_size,
             breakpoint_percentile_threshold=breakpoint_percentile_threshold,
@@ -202,9 +191,6 @@ def objective(trial, documents, ngram_size, question_ngrams, f_beta=1.0):
             embed_model=embed_model,
         )
     elif splitter_name == "markdown":
-        include_prev_next_rel = trial.suggest_categorical(
-            "include_prev_next_rel", [True, False]
-        )
         splitter = MarkdownNodeParser(
             include_prev_next_rel=include_prev_next_rel,
         )
@@ -218,7 +204,7 @@ def objective(trial, documents, ngram_size, question_ngrams, f_beta=1.0):
         "index",
         [
             "chromadb",
-            "milvus",  # need to create a (free) account at https://cloud.zilliz.com/
+            # "milvus",  # need to create a (free) account at https://cloud.zilliz.com/
             # and add MILVUS_URI=your public endpoint and MILVUS_TOKEN=your token (api key) to your .env file
         ],
     )
@@ -229,27 +215,7 @@ def objective(trial, documents, ngram_size, question_ngrams, f_beta=1.0):
             chroma_client.delete_collection("test")
         chroma_collection = chroma_client.create_collection("test")
         vector_store = ChromaVectorStore(chroma_collection=chroma_collection)
-    elif index_type == "milvus":
-        query_mode = VectorStoreQueryMode.HYBRID
-        milvus_k = trial.suggest_int("milvus_k", 40, 80)
-        # delete collection if it exists
-        client = MilvusClient(
-            uri=os.environ["MILVUS_URI"], token=os.environ["MILVUS_TOKEN"]
-        )
-        if client.has_collection("test"):
-            client.drop_collection(collection_name="test")
-        client.close()
-        # hybrid uses BGE-M3 for sparse vectors
-        vector_store = MilvusVectorStore(
-            uri=os.environ["MILVUS_URI"],
-            token=os.environ["MILVUS_TOKEN"],
-            collection_name="test",
-            dim=len(embed_model.get_text_embedding("foo")),
-            overwrite=True,
-            enable_sparse=True,
-            hybrid_ranker="RRFRanker",
-            hybrid_ranker_params={"k": milvus_k},
-        )
+
 
     # define top_k
     top_k = trial.suggest_int("top_k", 2, 50)
@@ -277,13 +243,29 @@ def objective(trial, documents, ngram_size, question_ngrams, f_beta=1.0):
     nodes = pipeline.run(documents=documents)
     # print('nodes', len(nodes))
 
-    # add the nodes to the index
+    # Inserta nodos en el índice
     index.insert_nodes(nodes)
 
-    # assert all nodes have been indexed
-    assert len(index.as_retriever(similarity_top_k=len(nodes)).retrieve("foo")) == len(
-        nodes
-    )
+    print(f"Nodes inserted: {len(nodes)}")
+
+    # Verificar si todos los nodos fueron indexados
+    # retrieved_nodes = index.as_retriever(similarity_top_k=len(nodes)).retrieve("foo")
+
+    # Si no se recuperan todos los nodos, reinserta los faltantes
+    # if len(retrieved_nodes) != len(nodes):
+    #     missing_nodes = [node for node in nodes if node.id_ not in [n.id_ for n in retrieved_nodes]]
+    #     print(f"Reindexing {len(missing_nodes)} missing nodes...")
+    #     index.insert_nodes(missing_nodes)
+
+    # # Verifica nuevamente que todos los nodos fueron indexados
+    # retrieved_nodes_after_reindex = index.as_retriever(similarity_top_k=len(nodes)).retrieve("foo")
+    # print(f"Number of nodes inserted: {len(nodes)}")
+    # print(f"Number of nodes retrieved: {len(retrieved_nodes_after_reindex)}")
+    # assert len(retrieved_nodes_after_reindex) == len(nodes), "Not all nodes were indexed!"
+
+
+
+    # assert len(retrieved_nodes) == len(nodes)
 
     # create a retriever from the index
     retriever = index.as_retriever(

54c08842b93cf08c79c0872f30a04179c3d816e2
IsaiasZc <coroxerox2@gmail.com>
2024-09-09 09:40:11 -0500

    [UPDATE] creating chunks

--- utils/retrieve.py
+++ utils/retrieve.py
@@ -29,6 +29,84 @@ def _generate_ngrams_from_text(text, ngram_size=3):
     ]
 
 
+# New Splitter
+
+from llama_index.core.schema import TextNode
+import spacy
+
+nlp = spacy.load("en_core_web_sm")
+
+def set_headers(par: str, header_levels: dict) -> dict:
+    """ Set headers for a paragraph """
+    
+    # Check if paragraph starts with a header (e.g. '# ', '## ', etc.)
+    for level in range(1, 7):
+        if par.startswith('#' * level + ' '):
+            header_levels[level] = par
+            # Reset lower-level headers
+            for lower_level in range(level + 1, 7):
+                header_levels[lower_level] = None
+            # Exit loop after finding the correct header level
+            break  
+    
+    # Build headers dictionary dynamically
+    headers = {f'header_{i}': header_levels[i] for i in range(1, 7) if header_levels[i]}
+    return headers
+
+def split_document_text(
+        paragraphs: list[str], 
+        md_metadata: dict,
+        add_metadata_to_text: bool = False,
+        split_by_sentence: bool = False
+    ) -> list[TextNode]:
+    """ Split text into paragraphs """
+    result = []
+    headers = {}
+    header_levels = {1: None, 2: None, 3: None, 4: None, 5: None, 6: None}
+    
+    for par in paragraphs:
+        headers = set_headers(par, header_levels)
+        # complet_context = set_complet_context(par, headers)
+        
+        metadata = {
+            **md_metadata,
+            **headers,
+            # 'paragraph': par,
+            # 'complet_context': complet_context
+        }        
+
+        # use spacy to split paragraph into sentences
+        if split_by_sentence:
+            doc = nlp(par)
+            for sent in doc.sents:
+                
+                if add_metadata_to_text:
+                    text = ''
+                    for value in metadata.values():
+                        text += str(value) + '\n'
+                    text += sent.text
+                else:
+                    text = sent.text
+                        
+                metadata['paragraph'] = par
+                node = TextNode(metadata=metadata, text=text)
+                result.append(node)
+
+        else:
+            if add_metadata_to_text:
+                text = ''
+                for value in metadata.values():
+                    text += str(value) + '\n'
+                text += par
+            else:
+                text = par
+            # Create a TextNode and add to result
+            metadata['paragraph'] = par
+            node = TextNode(metadata=metadata, text=text)
+            result.append(node)
+
+    return result
+
 def _generate_ngrams_from_texts(texts, ngram_size=3):
     """Generate all ngrams from a list of texts."""
 
@@ -165,9 +243,10 @@ def objective(trial, documents, ngram_size, question_ngrams, f_beta=1.0):
     splitter_name = trial.suggest_categorical(
         "splitter",
         [
-            "sentence",
-            "semantic",
-            "markdown",
+            #"sentence",
+            # "semantic",
+            #"markdown",
+            "paragraph"
         ],
     )
 
@@ -194,6 +273,9 @@ def objective(trial, documents, ngram_size, question_ngrams, f_beta=1.0):
         splitter = MarkdownNodeParser(
             include_prev_next_rel=include_prev_next_rel,
         )
+    elif splitter_name == "paragraph":
+        splitter = split_document_text
+
 
     # add metadata
     ## nothing for now

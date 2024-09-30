d2d184b54d999b575b658e918c5777b7e4060dfd
IsaiasZc <coroxerox2@gmail.com>
2024-09-30 14:00:19 -0500

    Working on Hyper to py files
    Co-authored-by: Alirio Mieres <Alirio-Mieres@users.noreply.github.com>

---
+++ notebooks/hyperparam/hyper.py
@@ -0,0 +1,124 @@
+import os
+import dotenv
+from llama_index.core import Document
+
+from llama_index.embeddings.openai import OpenAIEmbedding
+from llama_index.core.vector_stores.types import VectorStoreQueryMode
+from llama_index.vector_stores.chroma import ChromaVectorStore
+import chromadb
+
+dotenv.load_dotenv()
+
+from utils.hyper_functions import (
+    extract_index_metadata,
+    AltNodeParser,
+    run_pipeline
+)
+
+
+datapath = os.getenv("DATA_PATH")
+
+origin_paths = [f"{datapath}out/from_html/", f"{datapath}out/from_pdf/"]
+
+
+# Read the document names from the directories:
+files_list = [path + item for path in origin_paths for item in os.listdir(path)]
+
+files_list.sort()
+
+print("Files list length: ", len(files_list))
+
+documents = []
+
+for i, filepath in enumerate(files_list):
+    with open(filepath, "r", encoding="utf-8") as file:
+        document = Document(text=file.read(), metadata={"filepath": filepath})
+
+        # add the document to a single entry list
+        documents.append(document)
+
+
+documents = [extract_index_metadata(doc) for doc in documents]
+
+metadata_keys = set()
+for doc in documents:
+    for key in doc.metadata:
+        metadata_keys.add(key)
+
+
+print("Metadata added to documents")
+
+
+# **************** PROGRAM ***************
+
+embed_model_name = "text-embedding-3-large"
+
+embed_model = OpenAIEmbedding(
+            model=embed_model_name,
+            embed_batch_size=100,
+            max_retries=25,
+            timeout=180,
+            reuse_client=True,
+        )
+
+split_by = "paragraph"  ### trial.suggest_categorical("split_by", ["sentence", "paragraph", "both"])
+embed_prev_next_sentences = 0  ### trial.suggest_int("embed_prev_next_sentences", 0, 5)
+embed_prev_next_paragraphs = 1  ### trial.suggest_int("embed_prev_next_paragraphs", 1, 4)
+max_embed_length = 400
+# Two problems with embedding index headers:
+# 1. The index titles are often the exact same as our test questions, leading to data leakage
+#    which makes our numbers too good.
+# 2. The index titles may refer to specific parts of the page, but we associate them with the entire page
+#    which means that every chunk on the page appears equally relevant and gets our retriever confused.
+# So I think we'll have to rely on embedding markdown headers
+embed_index_headers = False
+embed_md_headers = True  ### trial.suggest_categorical("embed_md_headers", [True])
+include_prev_next_paragraphs = 2
+max_include_length = 700
+include_index_headers = False  ### trial.suggest_categorical("include_index_headers", [False])
+include_md_headers = True  ### trial.suggest_categorical("include_md_headers", [True])
+splitter_name = "alt_splitter"
+splitter = AltNodeParser().from_defaults(
+    split_by=split_by,
+    embed_prev_next_sentences=embed_prev_next_sentences,
+    embed_prev_next_paragraphs=embed_prev_next_paragraphs,
+    max_embed_length=max_embed_length,
+    embed_index_headers=embed_index_headers,
+    embed_md_headers=embed_md_headers,
+    include_prev_next_paragraphs=include_prev_next_paragraphs,
+    max_include_length=max_include_length,
+    include_index_headers=include_index_headers,
+    include_md_headers=include_md_headers,
+)
+
+
+# define index
+query_mode = VectorStoreQueryMode.DEFAULT
+index_type = "chromadb"
+
+chroma_client = chromadb.EphemeralClient()
+# delete collection if it exists
+if any(coll.name == "test" for coll in chroma_client.list_collections()):
+    chroma_client.delete_collection("test")
+chroma_collection = chroma_client.create_collection("test")
+vector_store = ChromaVectorStore(chroma_collection=chroma_collection)
+
+retriever_threshold = 0.0
+retriever_k = 35
+sparse_k = retriever_k * 5
+rerank_model = "rerank-lite-1"
+rerank_threshold = 0.21
+rerank_k = 17
+
+print("Starting pipeline")
+index, nodes = run_pipeline(documents, splitter, embed_model, vector_store, False)
+
+print("Pipeline finished")
+
+# create a retriever from the index
+retriever = index.as_retriever(
+    vector_store_query_mode=query_mode,
+    similarity_top_k=retriever_k,
+    sparse_top_k=sparse_k,
+)
+
---
+++ utils/hyper_functions.py
@@ -0,0 +1,497 @@
+import re
+from typing import Any, Optional
+
+from collections.abc import Sequence
+from llama_index.core.callbacks.base import CallbackManager
+import spacy
+from llama_index.core.node_parser.interface import NodeParser
+from llama_index.core.bridge.pydantic import Field
+from llama_index.core.schema import BaseNode, MetadataMode, TextNode
+from llama_index.core.ingestion import IngestionPipeline
+from llama_index.core.utils import get_tqdm_iterable
+from llama_index.core import VectorStoreIndex
+
+INDEX_METADATA_KEYS = [
+    "heading",
+    "subheading",
+]  # title often contains the question exactly and applies to only part of the document, "title"]
+MD_METADATA_KEYS = [
+    "header_1",
+    "header_2",
+    "header_3",
+    "header_4",
+    "header_5",
+    "header_6",
+]
+ORDERED_LIST_ITEM_PATTERN = r"^\s*\d+\.\s"
+UNORDERED_LIST_ITEM_PATTERN = r"^\s*[-*+]\s"
+
+
+class AltNodeParser(NodeParser):
+    """Alternate Node Parser"""
+
+    split_by: str = Field(
+        default="para",
+        description=("Split by sentence, paragraph, or both."),
+    )
+
+    embed_prev_next_sentences: int = Field(
+        default=0,
+        description=(
+            "Number of previous and next sentences to include when calculating embeddings."
+        ),
+    )
+
+    embed_prev_next_paragraphs: int = Field(
+        default=0,
+        description=(
+            "Number of previous and next paragraphs to include when calculating embeddings."
+        ),
+    )
+
+    max_embed_length: int = Field(
+        default=2048,
+        description=(
+            "Maximum length of text to embed. Used when embedding previous and next sentences or paragraphs."
+        ),
+    )
+
+    embed_index_headers: bool = Field(
+        default=False,
+        description=(
+            "Include headers from the index page when calculating embeddings."
+        ),
+    )
+
+    embed_md_headers: bool = Field(
+        default=False,
+        description=(
+            "Include headers from the markdown document when calculating embeddings."
+        ),
+    )
+
+    include_prev_next_paragraphs: int = Field(
+        default=0,
+        description=(
+            "Number of previous and next paragraphs to include in text for the LLM."
+        ),
+    )
+
+    max_include_length: int = Field(
+        default=2048,
+        description=(
+            "Maximum length of text to pass to the LLM. Used when including previous and next paragraphs."
+        ),
+    )
+
+    include_index_headers: bool = Field(
+        default=False,
+        description=("Include headers from the index page in text for the LLM."),
+    )
+
+    include_md_headers: bool = Field(
+        default=False,
+        description=("Include headers from the markdown document in text for the LLM."),
+    )
+
+    @classmethod
+    def from_defaults(
+        cls,
+        split_by: str = "para",
+        embed_prev_next_sentences: int = 0,
+        embed_prev_next_paragraphs: int = 0,
+        max_embed_length: int = 2048,
+        embed_index_headers: bool = False,
+        embed_md_headers: bool = False,
+        include_prev_next_paragraphs: int = 0,
+        max_include_length: int = 2048,
+        include_index_headers: bool = False,
+        include_md_headers: bool = False,
+        callback_manager: Optional[CallbackManager] = None,
+    ) -> "AltNodeParser":
+        callback_manager = callback_manager or CallbackManager([])
+
+        return cls(
+            split_by=split_by,
+            embed_prev_next_sentences=embed_prev_next_sentences,
+            embed_prev_next_paragraphs=embed_prev_next_paragraphs,
+            max_embed_length=max_embed_length,
+            embed_index_headers=embed_index_headers,
+            embed_md_headers=embed_md_headers,
+            include_prev_next_paragraphs=include_prev_next_paragraphs,
+            max_include_length=max_include_length,
+            include_index_headers=include_index_headers,
+            include_md_headers=include_md_headers,
+        )
+
+    @classmethod
+    def class_name(cls) -> str:
+        """Get class name."""
+        return "AltNodeParser"
+
+    def _parse_nodes(
+        self,
+        nodes: Sequence[BaseNode],
+        show_progress: bool = False,
+        **kwargs: Any,
+    ) -> list[BaseNode]:
+        all_nodes: list[BaseNode] = []
+        print("len of all_nodes", len(nodes))
+        nodes_with_progress = get_tqdm_iterable(nodes, show_progress, "Parsing nodes")
+        print("len of nodes_with_progress", len(nodes_with_progress))
+        for node in nodes_with_progress:
+            nodes = self.get_nodes_from_node(node, **kwargs)
+            all_nodes.extend(nodes)
+
+        return all_nodes
+
+    def get_nodes_from_node(self, node: BaseNode, **kwargs: Any) -> list[TextNode]:
+        """Split a node into a list of nodes."""
+
+        # turn node into a list of headers and paragraphs
+        headers_paragraphs = get_headers_and_paragraphs(node)
+        # get paragraph nodes (with metadata for headers and context text to send to the LLM)
+        nodes = get_paragraph_nodes(headers_paragraphs, node)
+        # include previous and next paragraphs
+        if self.include_prev_next_paragraphs > 0:
+            include_prev_next_contexts(
+                nodes, self.include_prev_next_paragraphs, self.max_include_length
+            )
+        # split by sentence, paragraph, or both
+        sentence_nodes = []
+        if self.split_by in ["sentence", "both"]:
+            sentence_nodes = get_sentences(nodes)
+            if self.embed_prev_next_sentences > 0:
+                sentence_nodes = embed_prev_next(
+                    sentence_nodes,
+                    self.embed_prev_next_sentences,
+                    self.max_embed_length,
+                )
+        if self.split_by == "sentence":
+            nodes = sentence_nodes
+        else:  # paragraph or both
+            if self.embed_prev_next_paragraphs > 0:
+                nodes = embed_prev_next(
+                    nodes, self.embed_prev_next_paragraphs, self.max_embed_length
+                )
+            if self.split_by == "both":
+                nodes.extend(sentence_nodes)
+        # embed index and/or markdown headers
+        if self.embed_index_headers or self.embed_md_headers:
+            keys = []
+            if self.embed_index_headers:
+                keys.extend(INDEX_METADATA_KEYS)
+            if self.embed_md_headers:
+                keys.extend(MD_METADATA_KEYS)
+            embed_metadata(nodes, keys)
+        # include index and/or markdown headers in text sent to the LLM
+        if self.include_index_headers or self.include_md_headers:
+            keys = []
+            if self.include_index_headers:
+                keys.extend(INDEX_METADATA_KEYS)
+            if self.include_md_headers:
+                keys.extend(MD_METADATA_KEYS)
+            include_metadata(nodes, keys)
+
+        return nodes
+
+
+def extract_index_metadata(node):
+    """Split the document text into headers and content."""
+
+    text = node.get_content(metadata_mode=MetadataMode.NONE)
+    parts = text.split("---\n")
+    # If there are no headers, return the document as-is
+    if len(parts) < 3:
+        return node
+
+    # Extract headers
+    headers_text = parts[1]
+
+    # Parse headers into a dictionary
+    headers = {}
+    current_key = None
+    for line in headers_text.strip().split("\n"):
+        if ": " in line:
+            key, value = line.split(": ", 1)
+            headers[key] = value
+            current_key = key
+        elif current_key is not None:
+            # This line is a continuation of the previous value
+            headers[current_key] += " " + line.strip()
+
+    # clean up titles
+    if "title" in headers:
+        title = headers["title"]
+        # Split the string using the corrected regex with escaped quotes
+        titles = re.split(r"\s*['\"]?\s*,\s*['\"]?\s*", title)
+
+        # Clean up each question by removing leading/trailing quotes and spaces
+        titles = [re.sub(r'^[\'"\s]+|[\'"\s]+$', "", t) for t in titles]
+
+        # Join the cleaned questions with the separator
+        headers["title"] = " | ".join(titles)
+
+    # Join the remaining parts as content
+    # TODO! don't join the last part if its empty - make sure we don't still have unnecessary --- separators
+    content = "---\n".join(parts[2:])
+
+    return TextNode(metadata=headers, text=content)
+
+
+def get_headers_and_paragraphs(node: BaseNode) -> list[str]:
+    """Get headers and paragraphs from a node."""
+
+    # get text from the node
+    text = node.get_content(metadata_mode=MetadataMode.NONE)
+    results = []
+    paragraph_lines = []
+    blank_line = False
+    # split text by newline
+    for line in text.split("\n"):
+        line = line.strip()
+        # is this a blank line? if so, add a newline to the paragraph
+        if len(line) == 0:
+            blank_line = True
+        # is this a header? if so, add the previous paragraph to the results and this line to the results
+        elif line.startswith("#"):
+            if len(paragraph_lines) > 0:
+                results.append("\n".join(paragraph_lines))
+                paragraph_lines = []
+            results.append(line)
+            blank_line = False
+        # add list items to the previous paragraph
+        elif re.match(ORDERED_LIST_ITEM_PATTERN, line) or re.match(
+            UNORDERED_LIST_ITEM_PATTERN, line
+        ):
+            if blank_line and len(paragraph_lines) > 0:
+                paragraph_lines.append("")
+            paragraph_lines.append(line)
+            blank_line = False
+        else:
+            # if this is a new paragraph, add the previous paragraph to the results
+            if blank_line and len(paragraph_lines) > 0:
+                results.append("\n".join(paragraph_lines))
+                paragraph_lines = []
+            paragraph_lines.append(line)
+            blank_line = False
+    if len(paragraph_lines) > 0:
+        results.append("\n".join(paragraph_lines))
+
+    # clean up the results
+    # remove leading/trailing whitespace from each result
+    results = [result.strip() for result in results]
+    # if a level 1 header has a single paragraph underneath it that's <= 5 words long and is followed by another level 1 header
+    # or if a level 1 header has no paragraph underneath it,
+    # it's probably not a header so we should convert it to a paragraph
+    for i in range(len(results) - 2):
+        if results[i].startswith("# ") and (
+            (len(results[i + 1].split()) <= 5 and results[i + 2].startswith("# "))
+            or results[i + 1].startswith("# ")
+        ):
+            results[i] = results[i].lstrip("# ")
+    # if two consecutive paragraphs are identical, remove the first one
+    for i in range(len(results) - 1):
+        if results[i] == results[i + 1]:
+            results[i] = ""
+    # join paragraphs that have <= 5 words with the previous paragraph
+    for i in range(1, len(results)):
+        if (
+            not results[i].startswith("#")
+            and not results[i - 1].startswith("#")
+            and len(results[i].split()) <= 5
+        ):
+            results[i] = f"{results[i-1]}\n{results[i]}"
+            results[i - 1] = ""
+    # only keep non-empty results
+    return [result for result in results if result]
+
+
+def update_headers(par: str, headers: dict) -> None:
+    """Set headers for a paragraph"""
+
+    # Check if paragraph starts with a header (e.g. '# ', '## ', etc.)
+    for level in range(1, 7):
+        if par.startswith("#" * level + " "):
+            headers[f"header_{level}"] = par.lstrip("# ")
+            # Reset lower-level headers
+            for lower_level in range(level + 1, 7):
+                sub_header = f"header_{lower_level}"
+                if sub_header in headers:
+                    del headers[sub_header]
+            # Exit loop after finding the correct header level
+            break
+
+
+def get_paragraph_nodes(
+    headers_paragraphs: list[str], doc_node: BaseNode
+) -> list[TextNode]:
+    """Get paragraph nodes with header metadata from a list of headers
+    and paragraphs and the original document node."""
+
+    nodes = []
+    headers = {}
+    doc_metadata = {key: value for key, value in doc_node.metadata.items()}
+    for par in headers_paragraphs:
+        # if this is a header, update the headers
+        if par.startswith("#"):
+            update_headers(par, headers)
+        else:
+            metadata = {
+                **headers,
+                **doc_metadata,
+                "context": par,
+            }
+            node = TextNode(metadata=metadata, text=par)
+            nodes.append(node)
+    return nodes
+
+
+def _equal_headers(metadata1: dict, metadata2: dict) -> bool:
+    """Check if two metadata dictionaries have the same headers."""
+
+    for key in MD_METADATA_KEYS:
+        if metadata1.get(key, "") != metadata2.get(key, ""):
+            return False
+    return True
+
+
+def include_prev_next_contexts(
+    nodes: list[TextNode], count: int, max_length: int
+) -> None:
+    """
+    Include up to count previous and next contexts in the context of each node
+    while they have the same headers and the combined length is less than the max length.
+    """
+
+    node_contexts = [node.metadata["context"] for node in nodes]
+    for i, node in enumerate(nodes):
+        node_context = node_contexts[i]
+        for j in range(1, count + 1):
+            prev_node = None if i - j < 0 else nodes[i - j]
+            next_node = None if i + j >= len(nodes) else nodes[i + j]
+            prev_context = ""
+            next_context = ""
+            if prev_node and _equal_headers(node.metadata, prev_node.metadata):
+                prev_context = node_contexts[i - j]
+            if next_node and _equal_headers(node.metadata, next_node.metadata):
+                next_context = node_contexts[i + j]
+            if len(prev_context) + len(node_context) + len(next_context) <= max_length:
+                node_context = (
+                    f"{prev_context}\n\n{node_context}\n\n{next_context}".strip()
+                )
+            else:
+                break
+        node.metadata["context"] = node_context
+
+
+def get_sentences(nodes: list[TextNode]) -> list[TextNode]:
+    """Get sentence nodes from paragraph nodes."""
+
+    nlp = spacy.load("en_core_web_sm")
+    sentence_nodes = []
+    for node in nodes:
+        doc = nlp(node.text)
+        for sent in doc.sents:
+            metadata = {key: value for key, value in node.metadata.items()}
+            sentence_nodes.append(TextNode(metadata=metadata, text=sent.text))
+    return sentence_nodes
+
+
+def embed_prev_next(
+    nodes: list[TextNode], count: int, max_length: int
+) -> list[TextNode]:
+    """
+    Include up to count previous and next texts in the text of each node
+    while they have the same headers and the combined length is less than the max length.
+    """
+
+    embed_nodes = []
+    for i, node in enumerate(nodes):
+        node_text = node.text
+        for j in range(1, count + 1):
+            prev_node = None if i - j < 0 else nodes[i - j]
+            next_node = None if i + j >= len(nodes) else nodes[i + j]
+            prev_text = ""
+            next_text = ""
+            if prev_node and _equal_headers(node.metadata, prev_node.metadata):
+                prev_text = prev_node.text
+            if next_node and _equal_headers(node.metadata, next_node.metadata):
+                next_text = next_node.text
+            if len(prev_text) + len(node_text) + len(next_text) <= max_length:
+                node_text = f"{prev_text}\n\n{node_text}\n\n{next_text}".strip()
+            else:
+                break
+        node = TextNode(
+            metadata={key: value for key, value in node.metadata.items()},
+            text=node_text,
+        )
+        embed_nodes.append(node)
+    return embed_nodes
+
+
+def embed_metadata(nodes: list[TextNode], metadata_keys: list[str]):
+    """Include metadata in the text of each node."""
+
+    for node in nodes:
+        headers = []
+        for key in metadata_keys:
+            value = node.metadata.get(key, "")
+            if value:
+                headers.append(value)
+        if len(headers) > 0:
+            node.text = f"{' / '.join(headers)}\n\n{node.text}"
+
+
+def include_metadata(nodes: list[TextNode], metadata_keys: list[str]):
+    """Include metadata in the context of each node."""
+
+    for node in nodes:
+        headers = []
+        for key in metadata_keys:
+            value = node.metadata.get(key, "")
+            if value:
+                headers.append(value)
+        if len(headers) > 0:
+            node.metadata["context"] = (
+                f"{' / '.join(headers)}\n\n{node.metadata.get('context', '')}"
+            )
+
+
+def run_pipeline(documents, splitter, embed_model, vector_store, include_prev_next_rel):
+    """Run the ingestion pipeline to split documents, generate embeddings, and insert into an index."""
+    pipeline = IngestionPipeline(
+        transformations=[
+            splitter,
+            embed_model,
+        ]
+    )
+    index = VectorStoreIndex.from_vector_store(
+        vector_store,
+        embed_model=embed_model,
+    )
+    nodes = pipeline.run(documents=documents, show_progress=False)
+
+    # if include_prev_next_rel:
+    # for i in range(0, len(nodes)):
+    #     if i > 0:
+    #         nodes[i].metadata["prev"] = nodes[i - 1].text
+    #     if i < len(nodes) - 1:
+    #         nodes[i].metadata["next"] = nodes[i + 1].text
+
+    url = nodes[0].metadata['url']
+    sequence = 1
+ 
+    for i in range(0, len(nodes)):
+        if url == nodes[i].metadata['url']:
+            nodes[i].metadata['sequence'] = sequence
+            sequence += 1
+        else:
+            url = nodes[i].metadata['url']
+            sequence = 1
+            nodes[i].metadata['sequence'] = sequence
+            sequence += 1
+
+    index.insert_nodes(nodes)
+    print(f"Nodes inserted: {len(nodes)}")
+    return index, nodes  # CHANGED

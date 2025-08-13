6fdd9fc026c8971c5f1b2b8501e6409dcc61a2dc
chrisedeson <edesonchristopher@gmail.com>
2025-08-13 21:08:32 +0100

    revert: update path for last_crawl_detail.json to be in the root data directory

--- main.py
+++ main.py
@@ -55,7 +55,8 @@ def main():
         "files_with_only_metadata": 0,
         "pdf_files_always_processed": 0,
     }
-    detail_json_path = os.path.join(DATA_PATH, "last_crawl_detail.json")
+    # Put last_crawl_detail.json in the root data directory (parent of DATA_PATH, adata...)
+    detail_json_path = os.path.join(os.path.dirname(DATA_PATH), "last_crawl_detail.json")
     output_data_path = os.path.join(DATA_PATH, "last_output_data.csv")
     detailed_log_path = os.path.join(DATA_PATH, "pipeline_detailed_log.jsonl")
 
